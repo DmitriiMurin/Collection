59dbe265e29aeb69cb439014778085c3dad5e7fa
DmitriiMurin <murin.dima@gmail.com>
2022-02-22 15:37:32 +0300

    Initial commit

---
+++ src/main/java/ArrayListEx1.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Maria");
+        System.out.println(arrayList1);
+    }
+}
